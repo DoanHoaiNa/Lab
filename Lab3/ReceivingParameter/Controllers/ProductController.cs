f0cecdd943c45bc53d94c829c3ac020721ad9a7c
Nam <dthdoanhoainam3125@gmail.com>
2025-04-28 10:28:47 +0700

    Done Lab3

---
+++ Lab3/ReceivingParameter/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ReceivingParameter.Models;
+
+namespace ReceivingParameter.Controllers
+{
+    public class ProductController : Controller
+    {
+        static List<Product> products = new List<Product>()
+        {
+            new Product(){Id = 1, Name = "Tao", Price = 1000},
+            new Product(){Id = 1, Name = "Nho", Price = 1500},
+            new Product(){Id = 1, Name = "Le", Price = 2000},
+            new Product(){Id = 1, Name = "Man", Price = 3000}
+        };
+        public IActionResult Details()  
+        {
+            return View(products);
+        }
+    }
+}
---
+++ Lab3/ReceivingParameter/Controllers/StudentController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ReceivingParameter.Models;
+
+namespace ReceivingParameter.Controllers
+{
+    public class StudentController : Controller
+    {
+        static List<Student> students = new List<Student>()
+
+        {
+            new Student(){Id = 1, Name = "Hoang Long", Address = "Thai binh"}
+        };
+        public IActionResult Index()
+        {
+            return View(students);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
